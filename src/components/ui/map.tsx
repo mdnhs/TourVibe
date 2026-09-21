94c3e695f80996a99d67b8094504092c6842546a
mdnhs <mdnhs.cse@gmail.com>
2026-09-21 20:27:45 +0600

    chore: add @types/geojson and fix GeoJSON import in map component

--- src/components/ui/map.tsx
+++ src/components/ui/map.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type { GeoJSON } from "geojson";
 import MapLibreGL, { type PopupOptions, type MarkerOptions } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import {
